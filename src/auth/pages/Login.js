98fc1beda4a80d4d521db4ebd564c9cf709191ea
Benjamin Iduwe <bencoderus@gmail.com>
2023-04-09 23:57:53 +0100

    Use component for page title

--- src/auth/pages/Login.js
+++ src/auth/pages/Login.js
@@ -3,6 +3,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthButton } from "../components/AuthButton";
 import { AuthSection } from "../components/AuthSection";
 import { attemptLogin, loginUser } from "../services/auth-service";
+import { PageTitle } from "../components/PageTitle";
 
 export const Login = () => {
   const [error, setError] = useState("");
@@ -43,32 +44,7 @@ export const Login = () => {
     <AuthSection>
       <div className="lg:w-1/2 xl:max-w-screen-sm">
         <div className="py-12 bg-gray-100 lg:bg-white flex justify-center lg:justify-start lg:px-12">
-          <div className="cursor-pointer flex items-center">
-            <div>
-              <svg
-                className="hidden lg:inline w-10 text-indigo-500"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                id="Layer_1"
-                x="0px"
-                y="0px"
-                viewBox="0 0 225 225"
-              >
-                <g transform="matrix( 1, 0, 0, 1, 0,0) ">
-                  <g>
-                    <path
-                      id="Layer0_0_1_STROKES"
-                      className="st0"
-                      d="M173.8,151.5l13.6-13.6 M35.4,89.9l29.1-29 M89.4,34.9v1 M137.4,187.9l-0.6-0.4     M36.6,138.7l0.2-0.2 M56.1,169.1l27.7-27.6 M63.8,111.5l74.3-74.4 M87.1,188.1L187.6,87.6 M110.8,114.5l57.8-57.8"
-                    />
-                  </g>
-                </g>
-              </svg>
-            </div>
-            <div className="text-2xl text-gray-800 tracking-wide ml-2 font-bold">
-              SpaceNote
-            </div>
-          </div>
+          <PageTitle/>
         </div>
         <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
           <h2
--- src/auth/pages/Signup.js
+++ src/auth/pages/Signup.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { AuthButton } from "../../auth/components/AuthButton";
 import { AuthSection } from "../../auth/components/AuthSection";
 import { register } from "../services/auth-service";
+import { PageTitle } from "../components/PageTitle";
 
 export const Signup = () => {
   const [form, setForm] = useState({});
@@ -25,7 +26,7 @@ export const Signup = () => {
 
     if (!response.status) {
       const error = response?.data?.error?.message || response?.data?.message;
-      
+
       setError(error);
       setLoading(false);
       return;
@@ -44,32 +45,7 @@ export const Signup = () => {
     <AuthSection>
       <div className="lg:w-1/2 xl:max-w-screen-sm">
         <div className="py-12 bg-gray-100 lg:bg-white flex justify-center lg:justify-start lg:px-12">
-          <div className="cursor-pointer flex items-center">
-            <div>
-              <svg
-                className="w-10 text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                id="Layer_1"
-                x="0px"
-                y="0px"
-                viewBox="0 0 225 225"
-              >
-                <g transform="matrix( 1, 0, 0, 1, 0,0) ">
-                  <g>
-                    <path
-                      id="Layer0_0_1_STROKES"
-                      className="st0"
-                      d="M173.8,151.5l13.6-13.6 M35.4,89.9l29.1-29 M89.4,34.9v1 M137.4,187.9l-0.6-0.4     M36.6,138.7l0.2-0.2 M56.1,169.1l27.7-27.6 M63.8,111.5l74.3-74.4 M87.1,188.1L187.6,87.6 M110.8,114.5l57.8-57.8"
-                    />
-                  </g>
-                </g>
-              </svg>
-            </div>
-            <div className="text-2xl text-gray-800 tracking-wide ml-2 font-semibold">
-              SpaceNote
-            </div>
-          </div>
+          <PageTitle />
         </div>
         <div className="mt-10 px-12 sm:px-24 md:px-48 lg:px-12 lg:mt-16 xl:px-24 xl:max-w-2xl">
           <h2
@@ -123,7 +99,11 @@ export const Signup = () => {
                 />
               </div>
               <div className="mt-10">
-              <AuthButton text="Create an account" type="submit" loading={loading} />
+                <AuthButton
+                  text="Create an account"
+                  type="submit"
+                  loading={loading}
+                />
               </div>
             </form>
             <div className="mt-12 text-sm font-display text-gray-700 text-center">
